492b73bf497e431f06f2ff4c672c10653a143bd6
Adhiraj Singh <adhiraj.bal@gmail.com>
2021-01-14 14:38:05 -0500

    12

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ fetch('https://api.thingspeak.com/channels/1265432/feeds/last/')
     document.getElementById("count").innerHTML = "Live Count: " + String(liveCount);
     document.getElementById("cap").innerHTML = "Max Capacity: " + String(maxCapacity);
     document.getElementById("space").innerHTML = "Space Left: " + String(spaceLeft);
+    if(maxCapacity != 0){
+
     google.charts.load('current', {packages: ['corechart']});
         google.charts.setOnLoadCallback(drawChart);
 
@@ -26,7 +28,7 @@ fetch('https://api.thingspeak.com/channels/1265432/feeds/last/')
           var chart = new google.visualization.PieChart(document.getElementById('piechart'));
           chart.draw(data, null);
         }
-
+      }
   })
   .catch((err) => {
     document.getElementById("Piechart").innerHTML = "Device Inactive";
